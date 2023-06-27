9f9d783a0208dd89de0f2d97706b2359b9c44402
zzj <15183299921@163.com>
2023-06-27 17:49:37 +0800

    暂存一下

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product, ProductService } from '../shared/product.service';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product',
@@ -9,7 +11,15 @@ import { Product, ProductService } from '../shared/product.service';
 export class ProductComponent implements OnInit {
   imgList = 'http://zhangblog.cn:7001/public/banner/banner3.jpg';
   products: Product[];
-  constructor(private productService: ProductService) {}
+  private keyword: string;
+  titleFilter = new FormControl();
+
+  constructor(private productService: ProductService) {
+    this.titleFilter.valueChanges.pipe(debounceTime(500)).subscribe((value) => {
+      this.keyword = value;
+    });
+  }
+
   ngOnInit(): void {
     this.products = this.productService.getProducts();
   }
